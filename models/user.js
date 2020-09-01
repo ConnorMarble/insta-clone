b5df6cae0c9bc6f215c23ad67d1f84385ae983de
Connor Marble <connor.marble@gmail.com>
2020-09-01 17:22:25 -0400

    first commit

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  resetToken: String,
+  expireToken: Date,
+  pic: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/instaclone-mernstack/image/upload/v1598979123/78-785827_user-profile-avatar-login-account-male-user-icon_zmqzlb.png",
+  },
+  followers: [{ type: ObjectId, ref: "User" }],
+  following: [{ type: ObjectId, ref: "User" }],
+});
+
+mongoose.model("User", userSchema);
